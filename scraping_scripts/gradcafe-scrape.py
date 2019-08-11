9bc72472290416401512d304188ae3baca2dc56c
danielazieba <danielaazieba@gmail.com>
2019-08-11 13:18:49 -0400

    Created basic GradCafe scraping script; pulls one post date

---
+++ scraping_scripts/gradcafe-scrape.py
@@ -0,0 +1,15 @@
+# scrapes data from thegradcafe.com
+import requests
+
+from bs4 import BeautifulSoup
+
+req_url = 'https://www.thegradcafe.com/survey/index.php?q='
+# in the future, allow script to feed in requested parameters to gradcafe
+# for now I'm leaving this part as is to test the scraping successfully
+university = 'Princeton+University'
+
+response = requests.get(req_url + university)
+html = response.content
+grad_soup = BeautifulSoup(html, "html.parser")
+
+print(grad_soup.find('td', attrs={'class':'tcol5'}))
