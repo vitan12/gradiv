76533d516f3607ff29f359609357ca6318d94953
danielazieba <danielaazieba@gmail.com>
2019-08-11 22:13:16 -0400

    Updated script to pull other page attributes

--- scraping_scripts/gradcafe-scrape.py
+++ scraping_scripts/gradcafe-scrape.py
@@ -16,10 +16,22 @@ grad_soup = BeautifulSoup(html, "html.parser")
 grad_json = {}
 grad_json['posts'] = []
 
-for index in range(len(grad_soup.find_all('td', attrs={'class':'tcol5'}))):
+uni = grad_soup.find_all('td', attrs={'class': 'tcol1'})
+type = grad_soup.find_all('td', attrs={'class': 'tcol2'})
+decision = grad_soup.find_all('td', attrs={'class': 'tcol3'})
+app_type = grad_soup.find_all('td', attrs={'class': 'tcol4'})
+dates = grad_soup.find_all('td', attrs={'class': 'tcol5'})
+comment = grad_soup.find_all('td', attrs={'class': 'tcol6'})
+
+for index in range(len(dates)):
 	grad_json['posts'].append(
 	{
-		'date': grad_soup.find_all('td', attrs={'class': 'tcol5'})[index].get_text()
+		'institution': uni[index].get_text(),
+		'program': type[index].get_text(),
+		'result': decision[index].get_text(),
+		'applicant_type': app_type[index].get_text(),
+		'date': dates[index].get_text(),
+		'comments': comment[index].get_text()
 	}
 	)
 
