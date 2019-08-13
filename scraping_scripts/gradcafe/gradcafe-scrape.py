65fcfc2a93eec0448fab34fafd21930a823c491c
Vishal <vishl.sriram@gmail.com>
2019-08-13 12:11:45 -0400

    Updated to take commandline args

--- scraping_scripts/gradcafe/gradcafe-scrape.py
+++ scraping_scripts/gradcafe/gradcafe-scrape.py
@@ -2,14 +2,21 @@
 import requests
 import json
 
+from sys import argv
 from bs4 import BeautifulSoup
+
 
 req_url = 'https://www.thegradcafe.com/survey/index.php?q='
 # in the future, allow script to feed in requested parameters to gradcafe
 # for now I'm leaving this part as is to test the scraping successfully
-university = 'Princeton+University'
 
-response = requests.get(req_url + university)
+
+def buildurl (uni):
+	uni.replace(" ", "+")
+	return req_url + uni
+
+print (argv[1])
+response = requests.get(buildurl(argv[1]))
 html = response.content
 grad_soup = BeautifulSoup(html, "html.parser")
 
@@ -38,4 +45,4 @@ for index in range(len(dates)):
 with open('gradcafe.json', 'w') as outfile:
 	json.dump(grad_json, outfile)
 
-print("done!")
+print("Finished!")
