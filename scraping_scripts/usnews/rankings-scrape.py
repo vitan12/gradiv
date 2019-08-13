f513b5db0aaa381fbae0b30be4bcf0c0a6aced65
Vishal <vishl.sriram@gmail.com>
2019-08-13 12:26:56 -0400

    Added working USNews scraping with updated HTML Finders

--- scraping_scripts/usnews/rankings-scrape.py
+++ scraping_scripts/usnews/rankings-scrape.py
@@ -9,7 +9,7 @@ rankings_list['rankings'] = []
 
 # Will add options to take in a variable URL based on what rankings to sort by
 # i.e. By tuition/field/acceptance rate. 
-url = 'https://www.usnews.com/best-graduate-schools/top-engineering-schools/eng-rankings?_mode=table'
+url = 'https://www.usnews.com/best-graduate-schools/top-engineering-schools/eng-rankings'
 colleges = []
 
 # Apparently this is necessary for USNews to not outright deny you
@@ -18,20 +18,21 @@ agent = {"User-Agent":"Mozilla/5.0"}
 r = requests.get(url, headers=agent)
 soup = BeautifulSoup(r.text)
 
-regex = '<a class="school-name" href="/best-colleges/(.+?)">[^.]*</a>'
-pattern = re.compile(regex)
+rank = soup.findAll('div', attrs={'class': 'ranklist-ranked-item RankList__Rank-f42a47-2 fTNmZK'})
+uni = soup.findAll('a', attrs={'class': 'Anchor-u1fur6-0 chvEFD'})
+location = soup.findAll('p', attrs={'class': 'Paragraph-fqygwe-0 bstttc'})
+tuition = soup.findAll('p', attrs={'class': 'fqygwe-0-Paragraph-hHEPzZ gscYiA'})
+college_page = soup.findAll('a', attrs={'href': 'Anchor-u1fur6-0 chvEFD'})
 
-rank = soup.findAll('span', attrs={'class': 'ranklist-ranked-item RankList__Rank'})
-uni = soup.findAll('a', attrs={'class': 'school-name'})
-location = soup.findAll('p', attrs={'class': 'location'})
-tuition = soup.findAll('span', attrs={'class': 'TuitionandFees'})
-college_page = soup.findAll(pattern, soup)
-
-print (uni)
+print (rank)
 
 for n in range (len(rank)):
     rankings_list['rankings'].append({
-            'University': uni[n].get_text()
+            #'University Name': uni[n].get_text(),
+            'Rank': rank[n].get_text(),
+            #'Location': location[n].get_text(),
+            'Tuition': tuition[n].get_text(),
+            #'USNews Website': college_page[n].get_text()
         })
 
 
