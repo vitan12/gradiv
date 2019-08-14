e697301a99d75fd52cf38760af69d78b0c1fa739
danielazieba <danielaazieba@gmail.com>
2019-08-14 15:58:56 -0400

    Created basic HTML page for use with Flask

--- gradiv-app/gradivflask/routes.py
+++ gradiv-app/gradivflask/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from gradivflask import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello world!"
+    return render_template('index.html')
