28791f3a916b395b1329f84de01636ac4d0cfac3
Vishal <vishl.sriram@gmail.com>
2019-08-15 22:45:55 -0400

    Basic database setup with mongodb

---
+++ test/connections.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Connects to the database
+mongoose.connect('mongodb://localhost/test');
+
+mongoose.connection.once('open', function(){
+    console.log('Connection Successful');
+
+}).on('error', function(error){
+    console.log('Connection Error', error);
+});
+
