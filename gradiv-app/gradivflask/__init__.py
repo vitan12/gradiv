1bdcbefb04c0160c734091253bcfb82574fd28e2
danielazieba <danielaazieba@gmail.com>
2019-08-15 20:41:16 -0400

    Integrated MongoDB with Flask app for basic query

--- gradiv-app/gradivflask/__init__.py
+++ gradiv-app/gradivflask/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/bestdb"
+mongo = PyMongo(app)
 
 from gradivflask import routes
--- gradiv-app/gradivflask/routes.py
+++ gradiv-app/gradivflask/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from gradivflask import app
+from flask_pymongo import PyMongo
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    mongo1 = PyMongo(app, uri="mongodb://localhost:27017/bestdb")
+    mongo_test = mongo1.db.testCol.find_one({"hello": "world"})
+    return render_template('index.html', test=mongo_test)
