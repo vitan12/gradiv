a07d5df6118a404af65a4d8661f8ab81093d9ab8
danielazieba <danielaazieba@gmail.com>
2019-08-16 00:44:45 -0400

    Created page modifying chartJS graph using GET URL

--- gradiv-app/gradivflask/routes.py
+++ gradiv-app/gradivflask/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 from gradivflask import app
 from flask_pymongo import PyMongo
 from datetime import time
@@ -16,3 +16,13 @@ def chart_test():
     labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
     values = [10, 9, 8, 7, 6, 4, 7, 8]
     return render_template('chart.html', values=values, labels=labels, legend=legend)
+
+@app.route("/get_test", methods=['GET', 'POST'])
+def get_test():
+    # test idea of using get requests to manipulate graph on page
+    # we will implement this first then integrate with the actual json
+    test = request.args.get('keyword')
+    legend = 'Monthly Data'
+    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
+    values = [10, 9, 8, 7, 6, 4, 7, 8]
+    return render_template('requests.html', curr_value=test, values=values, labels=labels, legend=test)
