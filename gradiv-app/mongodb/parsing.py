e9fd2e9e65e39f18d08f8e1374c58d37e88e4970
Vishal <vishl.sriram@gmail.com>
2019-08-17 11:44:41 -0400

    Updated parsingdb to work with json Array

--- gradiv-app/mongodb/parsing.py
+++ gradiv-app/mongodb/parsing.py
@@ -6,26 +6,34 @@ import re
 
 mongo = MongoClient(port=27017)
 db = mongo.gradb
+
+db.applications.drop()
+db.stats.drop()
+
 reviews = db['applications']
 university_stats = db['stats']
+
+
 
 with open('../scraping_scripts/gradcafe/gradcafe-multiple.json') as f:
     file_data = json.load(f)
 
-reviews.insert_many(file_data)  
+for d in file_data["posts"]:
+    reviews.insert(d)  
+#print(file_data)
 
 regex = re.compile('/accepted/', re.IGNORECASE)
 
 university = {
         'name' : "Princeton university",
-        'total_applications' : university_stats.count(),
-        'accepted' : university_stats.find({'result': regex}).count(),
-        'americans' : university_stats.find({'applicant_type': 'A'}).count(),
-        'usInt' : university_stats.find({'applicant_type': 'U'}).count(),
-        'international' : university_stats.find({'applicant_type': 'I'}).count(),
+        'total_applications' : reviews.count(),
+        'accepted' : reviews.find({'result': regex}).count(),
+        'americans' : reviews.find({'applicant_type': 'A'}).count(),
+        'usInt' : reviews.find({'applicant_type': 'U'}).count(),
+        'international' : reviews.find({'applicant_type': 'I'}).count(),
     }
 
 result = university_stats.insert_one(university)
 
-print ("Hello")
+print (university_stats.count())
 
